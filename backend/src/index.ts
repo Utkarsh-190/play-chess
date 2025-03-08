c48a4a50c34a6eaf4fe58c9778cf4e84fb214f60
Utkarsh Tyagi <utkarshtyagi62@gmail.com>
2025-03-08 13:01:26 +0530

    changed normal http server to express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,36 +2,47 @@ import { WebSocket, WebSocketServer } from 'ws';
 import { GameManager } from './GameManager';
 import https from "https";
 import http from "http";
+import express from "express";
+import cors from "cors";
 import fs from "fs";
 import path from "path";
 
 const PORT = process.env.PORT || 8080;
-const options = {
-  key: process.env.SSL_KEY || fs.readFileSync(path.join(__dirname, '../certs', 'key.pem')),
-  cert: process.env.SSL_CERT || fs.readFileSync(path.join(__dirname, '../certs', 'cert.pem'))
-};
+// const options = {
+//   key: process.env.SSL_KEY || fs.readFileSync(path.join(__dirname, '../certs', 'key.pem')),
+//   cert: process.env.SSL_CERT || fs.readFileSync(path.join(__dirname, '../certs', 'cert.pem'))
+// };
 
-const server = https.createServer(options, (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.write("This is the response from the server")
-    res.end();
-})
+const app = express();
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("response from express server");
+});
+const expressServer = app.listen(PORT, () => {
+  console.log("Express server is running on PORT: ", PORT);
+});
 
-const httpServer = http.createServer( (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.write("This is the response from the server")
-  res.end();
-})
+// const server = https.createServer(options, (req, res) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     res.write("This is the response from the server")
+//     res.end();
+// })
+
+// const httpServer = http.createServer( (req, res) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   res.write("This is the response from the server")
+//   res.end();
+// })
 
 const gameManager = new GameManager();
 
-const wss = new WebSocketServer({ server: httpServer });
+const wss = new WebSocketServer({ server: expressServer });
 console.log("websocket server started on PORT: ", PORT);
 wss.on('connection', function connection(ws : WebSocket) {
   console.log(`connection started for ${ws}`);
@@ -49,10 +60,10 @@ wss.on('connection', function connection(ws : WebSocket) {
   });
 });
  
-server.listen((8081), () => {
-  console.log("HTTPS server is Running on PORT: ", 8081);
-})
+// server.listen((8081), () => {
+//   console.log("HTTPS server is Running on PORT: ", 8081);
+// })
 
-httpServer.listen(PORT, () => { 
-  console.log("HTTP server is running on PORT: ", PORT);
-});
+// httpServer.listen(PORT, () => { 
+//   console.log("HTTP server is running on PORT: ", PORT);
+// });
